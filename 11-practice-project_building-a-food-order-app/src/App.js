031127f2dac54ba99f62efa8177c92479c99d261
Mohammad Rostami <mohammadeostami13@gmail.com>
2021-06-03 16:45:49 +0430

    Building a Food Order App

---
+++ 11-practice-project_building-a-food-order-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState} from "react";
+import './App.css';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvier from "./store/CartProvider";
+
+function App() {
+    const [cartIsShown,setCartIsShown]=useState(false);
+
+    const showCartHandler = () => {
+        setCartIsShown(true)
+    }
+    const hideCartHandler = () => {
+        setCartIsShown(false)
+    }
+
+    return (
+        <CartProvier>
+            {cartIsShown && <Cart onClose={hideCartHandler}/> }
+            <Header onShowCart={showCartHandler}/>
+            <main>
+                <Meals/>
+            </main>
+        </CartProvier>
+    );
+}
+
+export default App;
