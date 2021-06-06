8bef74b49926f6a5fb427f5097255ab8093dd14a
Mohammad Rostami <mohammadeostami13@gmail.com>
2021-06-06 14:01:43 +0430

    An Alternative Way Of Building Components_ Class-based Components

---
+++ 13-an-alternative-way-of-building-components_class-based-components/src/components/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import {Component} from "react";
+
+class ErrorBoundary extends Component{
+    constructor() {
+        super();
+        this.state ={
+            hasError:false
+        };
+    }
+    componentDidCatch(error, errorInfo) {
+        this.setState({hasError:true});
+    }
+    render() {
+        if (this.state.hasError) {
+            return <p>Some Thing is Wrong !</p>
+        }
+        return this.props.children
+    }
+}
+export default ErrorBoundary;
